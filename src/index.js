0a0d18bc43421258fda23363882936edaa6fe6e8
breno <brenokamura@gmail.com>
2022-11-10 14:14:10 -0300

    Armazenando os dados para

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import joi from 'joi';
 import { MongoClient } from "mongodb";
 
 dotenv.config();
@@ -14,5 +15,20 @@ let db;
 mongoClient.connect().then(() => {
 	db = mongoClient.db("batepapo");
 });
+
+const participantsSchema = joi.object({
+	name: joi.string().min(3).required()
+}
+);
+
+const messagesSchema = joi.object({
+	from: joi.string().required(),
+	to: joi.string().min(3).required(),
+	text: joi.string().min(1).required(),
+	type: joi.string().valid("message", "private_message").required(),
+	time: joi.string()
+}
+);
+
 
 app.listen(5000, () => console.log("Rodando a porta 5000. Sucesso!!!"))
